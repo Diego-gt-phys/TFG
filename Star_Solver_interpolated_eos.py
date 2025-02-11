bd8610a8bb1130c6a82adcfc6083a71fb8409808
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-02-11 10:41:45 +0100

    Cut the tail of p
    Made it so the minimum pressure is reachd when the central preassure drops to 10^-10 times its original value

--- Star_Solver_interpolated_eos.py
+++ Star_Solver_interpolated_eos.py
@@ -144,7 +144,7 @@ def runge_kutta_4th_order_with_stop(system, y0, r_range, h):
         y_next = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
         # Check if the stopping condition is met (p < 0)
-        if y_next[1] < 2e-16:  # p corresponds to y[1] DEBUG Está alargando mucho la cola.
+        if y_next[1] < y0[1]*1e-10:  # Beware of tail
             break
 
         r += h
@@ -214,35 +214,37 @@ def M_R_curve (pc_range, r_range, h, n):
 # Calculate the data
 ###############################################################################
 # Read the data
-data = pd.read_excel("stiff.xlsx")
+data = pd.read_excel("soft.xlsx")
 rho_data = data['Density'].values
 p_data = data['Pressure'].values
 
-# Find the solution for the TOV equation.
-R, M = M_R_curve((2.1e-6, 2.8e-4), (1e-6,20), 0.001, 20)
 
+
+# Find the solution for the TOV equation.
+r,m,p = TOV_solver([0,1.6191e-5], (1e-6,20), 0.001)
+print("La presión límite es:", p[-1])
+print("La masa de la estrella es:", m[-1], ", y su radio:", r[-1])
 ###############################################################################
 # Plot the data
 ###############################################################################
-
 plt.figure(figsize=(9.71, 6)) # The image follows the golden ratio
 colors = sns.color_palette("Set1", 5) # Generate a color palette
-plt.plot(R, M, label = r'stiff', color = colors[2], linewidth = 2, linestyle = '-', marker = '*', mfc='k', mec = 'k', ms = 6) # marker = '', mfc='k', mec = 'k', ms = 6
+plt.plot(r, p, label = r'soft', color = colors[0], linewidth = 2, linestyle = '-', marker = '', mfc='k', mec = 'k', ms = 6) # marker = '', mfc='k', mec = 'k', ms = 6
 
 # Set the axis to logarithmic scale
 #plt.xscale('log')
 #plt.yscale('log')
 
 # Add labels and title
-plt.title(r'Curva MR para la EOS: stiff', loc='left', fontsize=15, fontweight='bold')
-plt.xlabel(r'R $\left[km\right]$', fontsize=15, loc='center', fontweight='bold')
-plt.ylabel(r'M $\left[M_{\odot}/km^3\right]$', fontsize=15, loc='center', fontweight='bold')
+plt.title(r'Solución de TOV para la EOS: soft', loc='left', fontsize=15, fontweight='bold')
+plt.xlabel(r'r $\left[km\right]$', fontsize=15, loc='center', fontweight='bold')
+plt.ylabel(r'p $\left[M_{\odot}/km^3\right]$', fontsize=15, loc='center', fontweight='bold')
 plt.axhline(0, color='black', linewidth=1.0, linestyle='--')  # x-axis
 plt.axvline(0, color='black', linewidth=1.0, linestyle='--')  # y-axis
 
 # Set limits
-plt.xlim(8, 17)
-plt.ylim(0, 3.3)
+#plt.xlim(8, 17)
+#plt.ylim(0, 3.3)
 
 # Add grid
 plt.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
@@ -266,7 +268,7 @@ plt.gca().spines['left'].set_linewidth(1.5)
 plt.legend(fontsize=15, frameon=False) #  loc='upper right',
 
 # Save the plot as a PDF
-plt.savefig("MR_stiff.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("TOV.pdf", format="pdf", bbox_inches="tight")
 
 # Show the plot
 plt.tight_layout()
