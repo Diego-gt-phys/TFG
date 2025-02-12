e990011366e094ceb8292d827a1e49caac4ba5d2
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-02-12 13:42:59 +0100

    Reinstated the TOV figure.
    I accidentally over wrote on the file a lesser picture

--- Star_Solver_interpolated_eos.py
+++ Star_Solver_interpolated_eos.py
@@ -281,7 +281,7 @@ plt.gca().spines['left'].set_linewidth(1.5)
 plt.legend(fontsize=15, frameon=False) #  loc='upper right',
 
 # Save the plot as a PDF
-plt.savefig("TOV.pdf", format="pdf", bbox_inches="tight")
+#plt.savefig("TOV.pdf", format="pdf", bbox_inches="tight")
 
 # Show the plot
 plt.tight_layout()
