a614bda24ed97d7ec924eadd6d1c4d2de5e1a608
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-02-14 12:36:01 +0100

    Deleted the mA + mB plot.
    It was only a test to check that the total mass is the sum of both of them

--- two_fluid_solver_separate_mass.py
+++ two_fluid_solver_separate_mass.py
@@ -203,14 +203,13 @@ def TOV_solver(y0, r_range, h):
 r, m, p_A, p_B, m_A, m_B = TOV_solver((0, 0.9e-4, 1.4e-4, 0, 0), (1e-6, 20), 0.001)
 
 plt.figure(figsize=(9.71, 6))
-colors = sns.color_palette("Set1", 6) # Generate a color palette
+colors = sns.color_palette("Set1", 5) # Generate a color palette
 
 plt.plot(r, p_A*1e4, label = r'$p_A(r) \cdot 10^4$', color = colors[0], linewidth = 2, linestyle = '-') # , marker = "",  mfc='k', mec = 'k', ms = 6
 plt.plot(r, p_B*1e4, label = r'$p_B(r) \cdot 10^4$', color = colors[1], linewidth = 2, linestyle = '-') # , marker = "",  mfc='k', mec = 'k', ms = 6
 plt.plot(r, m, label = r'$m(r)$', color = colors[2], linewidth = 2, linestyle = '-') # , marker = "",  mfc='k', mec = 'k', ms = 6
-plt.plot(r, m_A, label = r'$m_A(r)$', color = colors[3], linewidth = 2, linestyle = '-.') # , marker = "",  mfc='k', mec = 'k', ms = 6
-plt.plot(r, m_B, label = r'$m_B(r)$', color = colors[4], linewidth = 2, linestyle = '-.') # , marker = "",  mfc='k', mec = 'k', ms = 6
-plt.plot(r, m_A + m_B, label = r'$m_A + m_B$', color = colors[5], linewidth = 1, linestyle = '-.') # , marker = "",  mfc='k', mec = 'k', ms = 6
+plt.plot(r, m_A, label = r'$m_A(r)$', color = colors[4], linewidth = 2, linestyle = '-.') # , marker = "",  mfc='k', mec = 'k', ms = 6
+plt.plot(r, m_B, label = r'$m_B(r)$', color = colors[3], linewidth = 2, linestyle = '-.') # , marker = "",  mfc='k', mec = 'k', ms = 6
 
 # Set the axis to logarithmic scale
 #plt.xscale('log')
