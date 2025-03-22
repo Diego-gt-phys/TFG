9c883b0e83407f61d3791be651113cba0c0fa24a
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-03-22 16:53:26 +0100

    Calculated some datasets

--- DANTE.py
+++ DANTE.py
@@ -618,7 +618,7 @@ def MR_curve_lambda (pc_range, l, r_range, h, n):
 
 print("Welcome to DANTE: the Dark-matter Admixed Neutron-sTar solvEr.")
 
-mode, d_type, eos_c, param_c, param_val, p_c = get_inputs(0, 3, 'middle', 'l', 0.5, 1e-5)
+mode, d_type, eos_c, param_c, param_val, p_c = get_inputs(1, 4, 'middle', 'l', 0.8, None)
 
 print("\nUser Inputs:", mode, d_type, eos_c, param_c, param_val, p_c)
 print("")
@@ -669,7 +669,7 @@ if mode == 1:
             eos_c = "DM"
             
         # Scale factors
-        p_scale = 4
+        p_scale = 5
         m_scale = 1
         
         # Configure the plot
@@ -744,7 +744,7 @@ if mode == 1:
         m_B = df["m_B"]
         
         # Scale factors
-        p_scale = 4
+        p_scale = 3
         m_scale = 1
         
         # Configure the plot
