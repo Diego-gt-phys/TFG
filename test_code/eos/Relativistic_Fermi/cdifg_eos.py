54ece53b621d34b20e07b90c7cf38beaccadefe2
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-04-04 23:25:13 +0200

    Started the process for relativistic eos
    In this scrip i'll try and create a function that opperates similar to the read_eos function in DANTE.py
    
    This function, due to the further liberty of dark matter masses will need to check if the excel file containing the eos exists, and if that is not the case, create a new one.
    
    For small values of P, i'll use the taylor expansion. Dont really know  why there is an issue but its something.

---
+++ test_code/eos/Relativistic_Fermi/cdifg_eos.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  4 22:03:40 2025
+
+Code used for creating the dataset conaining the information of the EoS of a Completely Degenerate Ideal Fermi Gas,
+whose particles have a guiven mass in the order of GeV.
+
+@author: Diego García Tejada
+"""
+
+def read_eos (dm_m): # Try and read the eos, if not calculate it.
+    
+    return None
+
