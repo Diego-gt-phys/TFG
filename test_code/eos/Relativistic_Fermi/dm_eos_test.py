73aae45f997d983a9b0d3cf6cf9df20ffe3569fd
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-04-04 13:36:40 +0200

    Succesfully tested the parametrization

--- test_code/eos/Relativistic_Fermi/dm_eos_test.py
+++ test_code/eos/Relativistic_Fermi/dm_eos_test.py
@@ -14,88 +14,117 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
 import matplotlib.lines as mlines
-import matplotlib.ticker as ticker
 import seaborn as sns
 
-def eos_B (p_B):
-    # We work with an Ideal Fermi Gass
-    Gamma = 5/3 # Polytropic coeficient for a degenetare (T=0) IFG
-    K = ((m)**(-8/3))*8.0165485819726 # Polytropic constant for a degenetare (T=0) IFG
+# I belive that numerically it fails at scales of x = 10^-10
 
-    #if p_B <= 0:
-        #return 0  # Avoid invalid values
-        
-    rho = (p_B / K) ** (1/Gamma)
-    return rho
+x = np.geomspace(1e-3, 10, 5000)
 
 def phi(x):
-    return (1 / (8 * np.pi**2)) * (x * np.sqrt(1 + x**2) * ((2/3) * x**2 - 1) + np.log(x + np.sqrt(1 + x**2)))
+    return np.sqrt(1 + x**2) * ((2/3) * x**3 - x) + np.log(x + np.sqrt(1 + x**2))
 
-def chi(x):
-    return (1 / (8 * np.pi**2)) * (x * np.sqrt(1 + x**2) * (1 + 2*x**2) - np.log(x + np.sqrt(1 + x**2)))
+def psi(x):
+    return np.sqrt(1 + x**2) * (2 * x**3 + x) - np.log(x + np.sqrt(1 + x**2))
 
-def alpha(m):
-    return m**4 * 4.7038237577114e-4
+def phi_9th(x):
+    return (8/15) * x**5 - (4/21) * x**7 + (1/9) * x**9
 
-m = 1
-x = np.logspace(-3.3, 1, 100)
+def psi_9th(x):
+    return (8/3) * x**3 + (4/5) * x**5 - (1/7) * x**7 + (1/18) * x**9
 
-y_1 = phi(x)
-y_2 = chi(x)
+def phi_nr(x):
+    return (8/15) * x**5
 
-p = alpha(m) * phi(x)
-rho = alpha(m) * chi(x)
+def psi_nr(x):
+    return (8/3) * x**3
 
-p_DANTE = np.logspace(-20, -1, 100)
-rho_DANTE = eos_B(p_DANTE)
+###############################################################################
+# Test of the code
+###############################################################################
 
-plt.figure(figsize=(9.71, 6))
-colors = sns.color_palette("Set1", 10)
+p = 4.7038237577116e-7 * phi(x)
+rho = 4.7038237577116e-7 * psi(x)
+p_9th = 4.7038237577116e-7 * phi_9th(x)
+rho_9th = 4.7038237577116e-7 * psi_9th(x)
+p_nr = 4.7038237577116e-7 * phi_nr(x)
+rho_nr = 4.7038237577116e-7 * psi_nr(x)
 
-plt.plot(rho, p, label=r'EoS', color=colors[0], linewidth=1.5, linestyle='-')
-plt.plot(rho_DANTE, p_DANTE, label=r'DANTE', color=colors[1], linewidth=1.5, linestyle='--')
+data = {0:[rho,p], 1:[rho_9th, p_9th], 2:[rho_nr, p_nr]}
 
+# Configure the plot
+fig, ax1 = plt.subplots(figsize=(9.71, 6))
+colors = sns.color_palette("Set1", 11)
+styles = ['-', '--', '-.']
+s=0
 
-plt.title(r'Equation of State of a Completely Degenerate, Ideal Fermi Gas', loc='left', fontsize=15, fontweight='bold')
-plt.xlabel(r'$\rho$ $\left[M_{\odot}/km^3\right]$', fontsize=15, loc='center')
-plt.ylabel(r'$p$ $\left[ M_{\odot}/km^3 \right]$', fontsize=15, loc='center')
-plt.axvline(0, color='k', linewidth=1.0, linestyle='--')
-plt.axhline(0, color='k', linewidth=1.0, linestyle='--')
+# Configure the axis
+ax1.set_xlabel(r'$\xi$', fontsize=15, loc='center')
+ax1.set_ylabel(r'$p$ $\left[ M_{\odot} / km^3 \right]$', fontsize=15, loc='center', color='k')
+ax1.tick_params(axis='y', colors='k')
+ax1.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax1.ticklabel_format(style='sci', axis='x', scilimits=(-3, 3))
+ax1.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax1.ticklabel_format(style='sci', axis='y', scilimits=(-3, 3))
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax2 = ax1.twinx()
+ax2.set_ylabel(r'$\rho$ $\left[ M_{\odot} / km^3 \right]$', fontsize=15, loc='center', color='k')
+ax2.tick_params(axis='y', colors='k')
+ax2.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax2.ticklabel_format(style='sci', axis='x', scilimits=(-3, 3))
+ax2.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+ax2.ticklabel_format(style='sci', axis='y', scilimits=(-3, 3))
+ax2.set_yscale('log')
 
-
-# Aplly scientific notation to y axis
-plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-plt.ticklabel_format(style='sci', axis='y', scilimits=(-3,3))
-plt.ticklabel_format(style='sci', axis='x', scilimits=(-3,3))
-
-# Set log scale for both axes
-#plt.xscale('log')
-#plt.yscale('log')
-
+# Plot
+for key in data.keys():
+    ax1.plot(x, data[key][1], color=colors[0], label=rf'$p_{{{key}}}$', linewidth=1.5, linestyle=styles[s]) #, marker = "+",  mfc='k', mec = 'k', ms = 5
+    ax2.plot(x, data[key][0], color=colors[1], label=rf'$\rho_{{{key}}}$', linewidth=1.5, linestyle=styles[s])
+    s += 1
+    
 # Set limits
-#plt.xlim(1e-15, 1e0)
-#plt.ylim(1e-20, 1e0)
+ax1.set_xlim(1e-2, 1e1)
+ax1.set_ylim(1e-18, 1e0)
+ax2.set_ylim(1e-18, 1e0)
 
-# Configure ticks for all four sides
-plt.tick_params(axis='both', which='major', direction='in', length=8, width=1.2, labelsize=12, top=True, right=True)
-plt.tick_params(axis='both', which='minor', direction='in', length=4, width=1, labelsize=12, top=True, right=True)
-plt.minorticks_on()
+# Configure ticks
+ax1.tick_params(axis='both', which='major', direction='in', length=8, width=1.2, labelsize=12, top=True)
+ax1.tick_params(axis='both', which='minor', direction='in', length=4, width=1, labelsize=12, top=True)
+ax1.minorticks_on()
+ax2.tick_params(axis='both', which='major', direction='in', length=8, width=1.2, labelsize=12, top=True, right=True)
+ax2.tick_params(axis='both', which='minor', direction='in', length=4, width=1, labelsize=12, top=True, right=True)
+ax2.minorticks_on()
 
-# Customize tick spacing 
-#plt.gca().set_xticks(np.arange(8, 17.1, 1))  # Major x ticks 
-#plt.gca().set_yticks(np.arange(0, 3.51, 0.5))  # Major y ticks 
+# Configure ticks spacing
+#ax1.set_xticks(np.arange(0, 2.51, 0.25))
+#ax1.set_xticks(np.arange(0, 9.6, 0.2), minor=True)
+ax1.set_yticks(np.geomspace(1e-18, 1e0, 10))
+#ax1.set_yticks(np.arange(0, 8.1e-5, 0.2e-5), minor=True)
+ax2.set_yticks(np.geomspace(1e-16, 1e0, 9))
+#ax2.set_yticks(np.arange(0, 1.01, 0.02), minor=True)
 
 # Set thicker axes
-plt.gca().spines['top'].set_linewidth(1.5)
-plt.gca().spines['right'].set_linewidth(1.5)
-plt.gca().spines['bottom'].set_linewidth(1.5)
-plt.gca().spines['left'].set_linewidth(1.5)
+for ax in [ax1, ax2]:
+    ax.spines['top'].set_linewidth(1.5)
+    ax.spines['right'].set_linewidth(1.5)
+    ax.spines['bottom'].set_linewidth(1.5)
+    ax.spines['left'].set_linewidth(1.5)
+    ax.spines['top'].set_color('k')
+    ax.spines['right'].set_color('k')
+    ax.spines['bottom'].set_color('k')
+    ax.spines['left'].set_color('k')
 
-plt.legend(fontsize=15, frameon=True, fancybox=False, ncol = 1, edgecolor="black", framealpha=1, labelspacing=0.2, handletextpad=0.3, handlelength=1.4, columnspacing=1)
+# Make legend
+handles_list = [
+    mlines.Line2D([], [], color=colors[0], linestyle='-', label=r"$p(\xi)$"),
+    mlines.Line2D([], [], color=colors[1], linestyle='-', label=r"$\rho(\xi)$"),
+    mlines.Line2D([], [], color='k', linestyle='-', label=r"Relativistic"),
+    mlines.Line2D([], [], color='k', linestyle='--', label=r"Quasi-Relativistic"),
+    mlines.Line2D([], [], color='k', linestyle='-.', label=r"Non-Relativistic")]
 
-# Save plot as PDF
+plt.legend(handles = handles_list, loc = "upper left", bbox_to_anchor=(0.009, 0.99), fontsize=15, frameon=True, fancybox=False, ncol = 1, edgecolor="black", framealpha=1, labelspacing=0.2, handletextpad=0.3, handlelength=1.4, columnspacing=1)
+
 plt.tight_layout()
-plt.savefig("dm_eos_test.pdf", format="pdf", bbox_inches="tight")
+plt.savefig("Parametrization_test_log.pdf", format="pdf", bbox_inches="tight")
 
 plt.show()
