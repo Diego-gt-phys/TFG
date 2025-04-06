3f0c01130fa80fadbbe4d87d086e72f82db52cda
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-04-06 11:44:12 +0200

    Creating new functions for DANTE integration

---
+++ test_code/eos/Relativistic_Fermi/cdifg_function__test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  6 11:37:30 2025
+
+Test of the two functions before adding them into Dante.py.
+
+@author: Diego García Tejada
+"""
+
+def read_dm_eos (dm_m):
+    
+    print("Try to read the excel file. If file is none existant then create and read it")
+    
+    return None
+
+def eos_B (p_B):
+    
+    print("Interpolate using the data of the excel file")
+    
+    return None
+
