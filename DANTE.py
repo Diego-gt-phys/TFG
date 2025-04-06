bb498d60a9692883ba700574ff73629cb9106148
Diego-gt-phys <diegogarciatejada12@gmail.com>
2025-04-06 18:08:27 +0200

    Relativistc implementation finished. Takes forever now

--- DANTE.py
+++ DANTE.py
@@ -64,22 +64,25 @@ def eos_A (p_A): # BM
 
 def eos_B (p_B): # DM
     """
-    Polytropic EOS for the star B. The polytropic constant and exponent are defined in the unit sections. 
-    
-    Parameters:
-        p_B (float): Preassure of the fluid B.
-        
-    Returns:
-        float: Density of the fluid B (rho) at preassure p_B.
-    """
-    # We work with an Ideal Fermi Gass
-    Gamma = 5/3 # Polytropic coeficient for a degenetare (T=0) IFG
-    K = ((dm_m)**(-8/3))*8.0165485819726 # Polytropic constant for a degenetare (T=0) IFG
+    Guiven the arrays 'p_data_dm' and 'rho_data_d,' which contain the information for the equation of state for the dark matter,
+    this funtion interpolates the value of rho for a guiven  p. 
 
+    Parameters
+    ----------
+    p_B : float
+        Preassure of fluid B at which we want to evaluate the eos.
+
+    Returns
+    -------
+    rho : float
+        Density associated to the preassure guiven.
+    """
     if p_B <= 0:
-        return 0  # Avoid invalid values
-        
-    rho = (p_B / K) ** (1/Gamma)
+        return 0
+    
+    interp_func = interp1d(p_data_dm, rho_data_dm, kind='linear', fill_value='extrapolate')
+    rho = interp_func(p_B)
+    
     return rho
 
 def system_of_ODE (r, y):
@@ -858,13 +861,96 @@ def Save_MR (s_type, eos_c, dm_m, p2_c, p2_v):
         
     return df
 
+def calc_CDIFG (dm_m):
+    """
+    Function that calculates the numerical values of the Relativistic Equation of State for a
+    Completley Degenerate, Ideal Fermi Gas of particle mass = dm_m.
+    
+    Due to numerical issues for values of P lower than 10^-12 it uses the 21st order taylor expansion.
+    
+    Parameters
+    ----------
+    dm_m : float
+        Mass of the particle mass in GeV.
+        
+    Returns
+    -------
+    Pandas DataFrame
+        DF containing all the datapoints of EoS.
+    """
+    def phi(x):
+        return np.sqrt(1 + x**2) * ((2/3) * x**3 - x) + np.log(x + np.sqrt(1 + x**2))
+    
+    def psi(x):
+        return np.sqrt(1 + x**2) * (2 * x**3 + x) - np.log(x + np.sqrt(1 + x**2))
+        
+    def phi_21st(x):
+        return (8/15) * x**5 - (4/21) * x**7 + (1/9) * x**9 - (5/66) * x**11 + (35/624) * x**13 - (7/160) * x**15 + (77/2176) * x**17 - (143/4864) * x**19 + (715/28672) * x**21
+    
+    def psi_21st(x):
+        return (8/3) * x**3 + (4/5) * x**5 - (1/7) * x**7 + (1/18) * x**9 - (5/176) * x**11 + (7/416) * x**13 - (7/640) * x**15 + (33/4352) * x**17 - (429/77824) * x**19 + (715/172032) * x**21
+    
+    k = (dm_m)**(4) * 1.4777498161008e-3
+        
+    x_vals = np.geomspace(1e-6, 10, 10000)
+    
+    pressures = []
+    densities = []
+    
+    for x in x_vals:
+        P_analytical = k * phi(x)
+        
+        if P_analytical >= 1e-12:
+            P = P_analytical
+            rho = k * psi(x)
+        else:
+            P = k * phi_21st(x)
+            rho = k * psi_21st(x)
+            
+        pressures.append(P)
+        densities.append(rho)
+        
+    df = pd.DataFrame({'rho': densities,'P': pressures})
+    
+    df.to_excel(rf'data_eos\eos_cdifg_{dm_m}.xlsx', index=False)
+        
+    return df
+
+def read_create_dm_eos (dm_m):
+    """
+    Reads an equation of state (EOS) file in Excel format and extracts density 
+    and pressure data of the dark matter.
+    
+    Parameters
+    ----------
+    dm_m : float
+        mass of the dark matter particle in GeV.
+        
+    Returns
+    -------
+    p_data_dm : numpy.ndarray
+        Array containing pressure values extracted from the EOS file.
+    rho_data_dm : numpy.ndarray
+        Array containing density values extracted from the EOS file.
+    """
+    try:
+        eos_data = pd.read_excel(f'data_eos\eos_cdifg_{dm_m}.xlsx')
+        rho_data_dm = eos_data['rho'].values
+        p_data_dm = eos_data['P'].values
+    except FileNotFoundError:
+        eos_data = calc_CDIFG(dm_m)
+        rho_data_dm = eos_data['rho'].values
+        p_data_dm = eos_data['P'].values
+        
+    return p_data_dm, rho_data_dm
+
 ###############################################################################
 # Define the parameters
 ###############################################################################
 
 print("Welcome to DANTE: the Dark-matter Admixed Neutron-sTar solvEr.")
 
-mode, s_type, d_type, eos_c, dm_m, p1_c, p1_v, p2_c, p2_v = get_inputs(0, 3, 0, 'soft', 1.5, 'M', 1.0, 'l', 0.25)
+mode, s_type, d_type, eos_c, dm_m, p1_c, p1_v, p2_c, p2_v = get_inputs(1, 3, 0, 'middle', 1.0, 'M', 1.0, 'l', 0.05)
 
 print(f"\nUser Inputs: {mode}, {s_type}, {d_type}, '{eos_c}', {dm_m}, '{p1_c}', {p1_v}, '{p2_c}', {p2_v}\n")
 
@@ -874,6 +960,7 @@ print(f"\nUser Inputs: {mode}, {s_type}, {d_type}, '{eos_c}', {dm_m}, '{p1_c}', 
 
 if mode == 0:
     p_data, rho_data = read_eos(eos_c)
+    p_data_dm, rho_data_dm = read_create_dm_eos(dm_m)
     
     if d_type == 0:
         df = Save_TOV(s_type, eos_c, dm_m, p1_c, p1_v, p2_c, p2_v)
